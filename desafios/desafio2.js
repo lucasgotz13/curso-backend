db0eb0e1088fa0a43ba69d122beec602657f1e9d
Lucas Gotz <tutigotz@gmail.com>
2023-12-27 20:29:02 -0300

    Desafio 2 completado

--- desafios/desafio2.js
+++ desafios/desafio2.js
@@ -58,31 +58,89 @@ class ProductManager {
             let data = JSON.parse(res);
             console.log(data);
         } catch (err) {
-            return console.log("No products were found");
+            console.log(this.products);
+            return;
         }
     }
 
     async getProductById(id) {
-        let res = await fs.readFile(this.path, "utf-8");
-        let data = JSON.parse(res);
-        let product = data.find((element) => id === element.id);
-        console.log(product ?? "No product exists with the requested id");
+        try {
+            let res = await fs.readFile(this.path, "utf-8");
+            let data = JSON.parse(res);
+            let product = data.find((element) => id === element.id);
+            console.log(product ?? "No product exists with the requested id");
+        } catch (err) {
+            console.log("No products were found");
+            return;
+        }
     }
 
-    async deleteProducts() {
+    async updateProduct(id, { ...product }) {
+        try {
+            let res = await fs.readFile(this.path, "utf-8");
+            let data = JSON.parse(res);
+            this.products = data;
+
+            let productIndex = data.findIndex((product) => id === product.id);
+
+            if (productIndex !== -1) {
+                this.products[productIndex] = {
+                    ...this.products[productIndex],
+                    ...product,
+                };
+            } else {
+                console.log("The product with the following id wasn't found");
+            }
+            await fs.writeFile(this.path, JSON.stringify(this.products));
+        } catch (err) {
+            console.log(
+                "No products were found. Instead try adding a new product"
+            );
+            return;
+        }
+    }
+
+    async deleteProduct(id) {
+        try {
+            let res = await fs.readFile(this.path, "utf-8");
+            let data = JSON.parse(res);
+            this.products = data;
+
+            let productIndex = data.findIndex((product) => id === product.id);
+
+            if (productIndex !== -1) {
+                this.products.splice(productIndex, 1);
+                await fs.writeFile(this.path, JSON.stringify(this.products));
+            } else {
+                console.log("The product with the following id does not exist");
+            }
+        } catch (err) {
+            console.log("No products were found");
+        }
+    }
+
+    async unlinkProducts() {
         await fs.unlink(this.path);
     }
 }
 
+// PROCESO DE TESTING
+
 const productManager = new ProductManager("./products.json");
 // productManager.addProduct({
-//     title: "producto prueba 4",
+//     title: "producto prueba",
 //     description: "Este es un producto prueba",
 //     price: 200,
 //     thumbnail: "Sin imagen",
-//     code: "abc126",
+//     code: "abc123",
 //     stock: 25,
 // });
 // productManager.getProducts();
-// productManager.getProductById(10);
-productManager.deleteProducts();
+// productManager.getProductById(1);
+// productManager.updateProduct(1, {
+//     title: "test 2",
+//     description: "test description",
+//     price: 1000,
+// });
+// productManager.deleteProduct(1);
+productManager.unlinkProducts();
